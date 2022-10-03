1dd0262dd6b9d41a317157e83d30fb99debab2e4
aubec-q4 <chris@chrisaube.com>
2022-10-03 15:27:58 -0400

    UPDATE - added complete color palette
    - improved fonts

--- dev/src/index.tsx
+++ dev/src/index.tsx
@@ -1,13 +1,46 @@
 import React, { useState } from 'react';
 import { createRoot } from 'react-dom/client';
 import { Theme, Button } from '../../src';
-import './index.css';
+import './_index.scss';
 
 const container = document.getElementById('app') as HTMLElement;
 const root = createRoot(container);
 
+export type ColorBlockProps = {
+  color: string;
+};
+
+const ColorBlock = ({ color }: ColorBlockProps) => {
+  const classes = `color-block__display ${color}`;
+
+  return (
+    <div className='color-block'>
+      <div className={classes}></div>
+      <div className='color-block__label'>{color.replace('--', ' ')}</div>
+    </div>
+  );
+};
+
+const ColorSection = ({ color }: ColorBlockProps) => {
+  const shades = [50, 100, 200, 300, 400, 500, 600, 700, 800, 900, 1000];
+
+  return (
+    <div className='color-section'>
+      <h2 className='color-section__label'>{color}</h2>
+      <div className='color-section__blocks'>
+        {shades.map((shade: number, idx: number) => {
+          const colorShade = `${color}--${shade}`;
+
+          return <ColorBlock color={colorShade} key={idx} />;
+        })}
+      </div>
+    </div>
+  );
+};
+
 const App = () => {
   const [theme, setTheme] = useState<Theme.Themes>('light');
+  const colors = ['grey', 'green', 'yellow', 'orange', 'red', 'rose', 'pink', 'purple', 'indigo', 'blue', 'cyan', 'teal'];
 
   const handlerTheme = () => {
     const themeToggle = theme === 'light' ? 'dark' : 'light';
@@ -17,7 +50,10 @@ const App = () => {
 
   return (
     <Theme.Provider theme={theme}>
-      <Button className="theme-btn" onClick={handlerTheme}>Toggle Theme: {theme}</Button>
+      <Button className="theme-btn text-lg" onClick={handlerTheme}>Toggle Theme: {theme}</Button>
+      {colors.map((color: string, idx: number) => {
+        return <ColorSection color={color} key={idx} />;
+      })}
     </Theme.Provider>
   );
 }
