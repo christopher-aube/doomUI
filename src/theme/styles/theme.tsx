ad9ce523bc4e8fd514547dcdee67b2c5c466d9ca
aubec-q4 <chris@chrisaube.com>
2022-10-03 13:16:15 -0400

    UPDATE - implemented styles as scss

--- src/theme/styles/theme.tsx
+++ src/theme/styles/theme.tsx
@@ -1,37 +1,13 @@
-import * as vars from './variables.css';
-import * as typography from './typography.css';
-import * as dark from './theme-dark.css';
-import * as light from './theme-light.css';
+import * as styles from './_index.scss';
 import * as utils from '../utils';
 
-const varsKeys = Object.keys(vars);
-const typeKeys = Object.keys(typography);
-const darkKeys = Object.keys(dark);
-const lightKeys = Object.keys(light);
+const styleKeys = Object.keys(styles);
 
 export const lookup = (style: string) => {
-  const [isVar, varName] = utils.searchStyles(style, varsKeys, vars);
+  const [hasStyle, styleName] = utils.searchStyles(style, styleKeys, styles);
 
-  if (isVar) {
-    return varName;
-  }
-
-  const [isType, typeName] = utils.searchStyles(style, typeKeys, typography);
-
-  if (isType) {
-    return typeName;
-  }
-
-  const [isDark, darkName] = utils.searchStyles(style, darkKeys, dark);
-
-  if (isDark) {
-    return darkName;
-  }
-
-  const [isLight, lightName] = utils.searchStyles(style, lightKeys, light);
-
-  if (isDark) {
-    return darkName;
+  if (hasStyle) {
+    return styleName;
   }
 
   return '';
@@ -39,8 +15,5 @@ export const lookup = (style: string) => {
 
 export default {
   lookup,
-  vars,
-  typography,
-  dark,
-  light,
+  styles,
 };
